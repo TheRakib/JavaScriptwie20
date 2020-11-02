1cff44f91196287367ebb01295e788cd42c9a9c5
RakibTest <rakibmedieinstitutet@gmail.com>
2020-11-02 12:00:29 +0100

    javascript första lektion

---
+++ app.js
@@ -0,0 +1,80 @@
+//fråga user att mata in 2 siffror 
+
+// Vi kommer att visa summan av dem 2 siffrona
+  // användare har matat in 
+
+
+ // fråga användare ?? med prompt() // istället för form/input ska vi använda prompt 
+
+ 
+ //lagra info/data : i var  //string 
+      //convertera till int
+  
+
+/* var number1=  prompt("Mata in en siffra")
+var number2 = prompt("mata in andra siffran")
+
+  //var convertedNumber1=  Number(number1)
+  //var convertedNumber2=  Number(number2)
+ // var summan=  convertedNumber1 + convertedNumber2
+// plus/ addera dem
+
+// tar vi inte emot någon minus siffror 
+
+
+ var summan=  Number(number1) + Number(number2)
+
+
+ alert(summan) 
+ */
+// visa till användare
+
+
+
+//  *********************  //
+// program 2: ta in betygsinfo  från användare och 
+   //lagra in i en lista. 
+
+ var fysikBetygsGrad = prompt("mata in fysiks betyg");
+
+// rad efter rad översätts 
+
+
+// Vi ska inte ta något betyggrad som är mindre än 2 
+
+if(fysikBetygsGrad >=2 ) {
+var betygsLista = [fysikBetygsGrad]
+   //alert(betygsLista) 
+   alert("Grattis " + betygsLista)
+}
+else {
+    alert("betyget är för lågt")
+}
+
+
+
+
+ 
+
+    
+
+
+
+
+// varje if statement körs/översätts av maskin 
+// else if körs bara om ovantående vilkoret inte är sant 
+
+/*  else if( false) {
+  
+alert("hej , den här raden kommer att köras om övanstådende vilkoret inte är sant")
+
+}
+ */
+
+ //hur ska vi hämta value från en array
+ 
+ 
+ //nameGivningConvention: 
+
+ //  var infoGradeKemi= 5;
+
