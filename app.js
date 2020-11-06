c667ed63986e8dd1d411fd70fc417a0eb53f565a
RakibTest <rakibmedieinstitutet@gmail.com>
2020-11-06 10:20:52 +0100

    for loop

--- app.js
+++ app.js
@@ -1,80 +1,82 @@
-//fråga user att mata in 2 siffror 
-
-// Vi kommer att visa summan av dem 2 siffrona
-  // användare har matat in 
-
-
- // fråga användare ?? med prompt() // istället för form/input ska vi använda prompt 
-
- 
- //lagra info/data : i var  //string 
-      //convertera till int
+// for ( start; villkor;  ökning/minskning)
+ // {  kod raderna här kommer att köras så länge vilkoret är sant}
   
+ // i--   i= i+1 i+=1  // i-=2 
 
-/* var number1=  prompt("Mata in en siffra")
-var number2 = prompt("mata in andra siffran")
-
-  //var convertedNumber1=  Number(number1)
-  //var convertedNumber2=  Number(number2)
- // var summan=  convertedNumber1 + convertedNumber2
-// plus/ addera dem
-
-// tar vi inte emot någon minus siffror 
-
-
- var summan=  Number(number1) + Number(number2)
-
-
- alert(summan) 
- */
-// visa till användare
-
-
-
-//  *********************  //
-// program 2: ta in betygsinfo  från användare och 
-   //lagra in i en lista. 
-
- var fysikBetygsGrad = prompt("mata in fysiks betyg");
-
-// rad efter rad översätts 
-
-
-// Vi ska inte ta något betyggrad som är mindre än 2 
-
-if(fysikBetygsGrad >=2 ) {
-var betygsLista = [fysikBetygsGrad]
-   //alert(betygsLista) 
-   alert("Grattis " + betygsLista)
-}
-else {
-    alert("betyget är för lågt")
-}
-
-
-
-
+/* for (var i = 0; i < 10; i = i+3) {
  
-
-    
-
-
-
-
-// varje if statement körs/översätts av maskin 
-// else if körs bara om ovantående vilkoret inte är sant 
-
-/*  else if( false) {
-  
-alert("hej , den här raden kommer att köras om övanstådende vilkoret inte är sant")
+   console.log(i)
+   //0 
+   //3
+   //6
+   //9
 
 }
  */
 
- //hur ska vi hämta value från en array
- 
- 
- //nameGivningConvention: 
 
- //  var infoGradeKemi= 5;
+ var lista= [0, 1, 2,3,4,5,6,7, 8,9,10]
+ //lista.length  11; 
+       // visa udda tal från den här listan 
+      for(var i= 0; i< lista.length; i+=2)
+      {
 
+         //if sats
+          console.log(  lista[i+1]  )
+          // hur vi kan visa value en array ??
+            //lista[0] // 0
+          //lista[1] // 1
+      }
+
+//Uppgiften 1:  
+// skapa en funktion som kollar upp användarens matat in 
+  //lösenord  och upprepande lösenord är samma
+  // till ex. två input fält , en kommer att ta in lösenord, 
+  //upprepande lösenord
+
+  // om lösenordet inte matchar visar den något fel meddelande
+  // om lösenordet matchar visar den succee // div  / alert()
+  //se till om ni har formulär , eller två input 
+  // om ni har förmulär glöm inte e.preventDefault();
+
+
+ // Uppgift 2: Visa udda tal från listan ,   //minst två stycken lösningsätt 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let x = 3;
+//y = x++;
+
+// y = 3
+// x = 4
+
+//var i = 0;
+
+//  ++1
+//--1
+
+  // i = i-1; //?? = -1 i--
+
+   // 
+
+
+   // 9.40 
+   
+ 
+  // i += 1
+
+
+
+   // 
