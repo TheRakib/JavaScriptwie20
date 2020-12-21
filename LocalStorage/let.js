c67321ff31900f07d55fc1a6cd0224c8ef4040e0
RakibTest <rakibmedieinstitutet@gmail.com>
2020-12-21 16:23:08 +0100

    prov förberedning

---
+++ LocalStorage/let.js
@@ -0,0 +1,27 @@
+
+//variable deklaration ? :- 
+
+//var name ;
+
+// tilldelning /initializing 
+
+//name = " Rakib"
+
+
+let name = "Rakib"
+
+name = "något annat "
+
+//man kan inte deklarera med samma variabel namn 
+
+console.log(name)
+
+
+const value = 100;
+
+
+
+// ej går : deklarera samma namn , initialera nya value 
+console.lop(value) 
+
+
